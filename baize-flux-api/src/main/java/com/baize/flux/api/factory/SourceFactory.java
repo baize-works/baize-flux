c466cf92bed91f2040985b277f54873f42d0a98b
baize <78582861+baize-works@users.noreply.github.com>
2026-07-23 10:39:47 +0800

    feat(engine): implement local JDBC bounded sync pipeline

---
+++ baize-flux-api/src/main/java/com/baize/flux/api/factory/SourceFactory.java
@@ -0,0 +1,6 @@
+package com.baize.flux.api.factory;
+import com.baize.flux.api.configuration.OptionRule;
+import com.baize.flux.api.configuration.ReadonlyConfig;
+import com.baize.flux.api.source.BoundedSource;
+import com.baize.flux.api.table.FluxRow;
+public interface SourceFactory { String factoryIdentifier(); OptionRule optionRule(); BoundedSource<FluxRow, ?> createSource(ReadonlyConfig config); }
